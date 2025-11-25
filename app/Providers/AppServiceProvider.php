5adbbac1ed11e3fac171fe6fce09098091c0ed56
HamidKhaeruman <hamidkhaeruman28@gmail.com>
2025-11-25 20:12:00 +0700

    fix: Add missing import for Validator in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 use App\Services\GoogleSheetService;
+use Illuminate\Support\Facades\Validator;
 
 class AppServiceProvider extends ServiceProvider
 {
